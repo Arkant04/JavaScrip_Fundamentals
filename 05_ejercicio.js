58509e36debbf69a83993ceefc4de465edf8ad0d
Alejandro <delacorte19@gmail.com>
2023-10-22 19:56:08 +0200

    MOD: Solucionando ejercicio 5

--- 05_ejercicio.js
+++ 05_ejercicio.js
@@ -2,7 +2,7 @@ function areaCirculo()
 {
   let radio= 42
    area = Math.PI * radio * radio;
-   console.log("El area del radio" + "" + radio + " " + "es:");
+   console.log("El area del radio" + "" + " " + "es:" + radio);
        
 }
 areaCirculo()
