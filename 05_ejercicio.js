209653a1f1a4463bc88d4ff16d47c8cca2bdb4b2
Alejandro <delacorte19@gmail.com>
2023-10-22 17:46:31 +0200

    MOD: Se añade el ejercicio 5 pero este no esta completo todavia

---
+++ 05_ejercicio.js
@@ -0,0 +1,9 @@
+Function.areaCirculo(radio) 
+{
+   
+   var area = Math.PI * radio * radio;
+   console.log("El area del radio" + radio + "es: " + area);
+   return area;     
+}
+areaCirculo(19);
+var radio = 1;
