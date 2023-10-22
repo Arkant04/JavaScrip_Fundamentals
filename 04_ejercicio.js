fd8370aee73ef1e86231c1adc076b52b05420e5f
Alejandro <delacorte19@gmail.com>
2023-10-22 17:23:10 +0200

    MOD: Se ha añadido otro ejercicio

---
+++ 04_ejercicio.js
@@ -0,0 +1,12 @@
+sumar = 2 + 2
+restar = 4 - 3
+multiplicar = 3 * 5
+dividir = 10 / 5
+doble = 19 * 2
+
+console.log(sumar)
+console.log(restar)
+console.log(multiplicar)
+console.log(dividir)
+console.log(doble)
+
