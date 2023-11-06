299a0d838c824e94165f4f7cf74a839ddf1ada40
Alejandro <delacorte19@gmail.com>
2023-11-07 00:57:17 +0100

    MOD: Ejercicio 16 y 17 terminados  😎

---
+++ ejercicio_16.js
@@ -0,0 +1,7 @@
+function lasLetraCambianParte1(){
+    let ñ = "hallot welt"
+    console.log(ñ.replace("hallot welt", "Hallot Welt"))
+
+}
+
+lasLetraCambianParte1()
---
+++ ejercicio_17.js
@@ -0,0 +1,7 @@
+function lasLetraCambianParte2(){
+    let n = "hallot welt"
+    console.log(n.replace("hallot welt", "hAllOt wElT"))
+
+}
+
+lasLetraCambianParte2()
